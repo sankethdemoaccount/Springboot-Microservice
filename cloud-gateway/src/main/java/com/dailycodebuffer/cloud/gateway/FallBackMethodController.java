71ca7a3af4dbff4441729764c1ad0a5fba586df3
sankethdemoaccount <106182643+sankethdemoaccount@users.noreply.github.com>
2022-09-22 15:01:54 +0530

    Update FallBackMethodController.java

--- cloud-gateway/src/main/java/com/dailycodebuffer/cloud/gateway/FallBackMethodController.java
+++ cloud-gateway/src/main/java/com/dailycodebuffer/cloud/gateway/FallBackMethodController.java
@@ -9,12 +9,12 @@ public class FallBackMethodController {
     @GetMapping("/userServiceFallBack")
     public String userServiceFallBackMethod() {
         return "User Service is taking longer than Expected." +
-                " Please try again later";
+                " Please try again ";
     }
 
     @GetMapping("/departmentServiceFallBack")
     public String departmentServiceFallBackMethod() {
         return "Department Service is taking longer than Expected." +
-                " Please try again later ";
+                " Please try again  ";
     }
 }
